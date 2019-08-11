7cb63a6c690ea31d5c2368c64eff879ef273bf38
Jessica Temporal <jessicatemporal@gmail.com>
2019-08-11 10:42:39 -0300

    movendo o modulo src para o padrao do cookiecutter

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,13 @@
 import re
 
-from src.parses import parse_pagina
+from bs4 import BeautifulSoup
+from requests import get
+
+
+def parse_pagina(url):
+    req = get(url)
+    decodificado = req.content.decode('utf-8')
+    return BeautifulSoup(decodificado, 'html.parser')
 
 
 class ColetorLinksPizza():
@@ -9,7 +16,7 @@ class ColetorLinksPizza():
         self.links = []
         self.sopa = ''
         self.prox_pag = ''
-        
+
     def coleta_links_episodios(self, sopa_de_letras=None):
         r = re.compile('Episódio')
         if not sopa_de_letras:
@@ -23,12 +30,12 @@ class ColetorLinksPizza():
         self.prox_pag = s.find_all('a', text=re.compile('Older'))
         if self.prox_pag:
             self.prox_pag = self.prox_pag[0].get('href')
-    
+
     def arrasta(self):
         self.sopa = parse_pagina(self.url)
         self.coleta_links_episodios()
         self.procura_proxima_pagina()
-        
+
         while self.prox_pag:
             self.sopa = parse_pagina(self.prox_pag)
             self.procura_proxima_pagina(self.sopa)
--- src/parses.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-from requests import get
-
-
-def parse_pagina(u):
-    req = get(u)
-    decodificado = req.content.decode('utf-8')
-    return BeautifulSoup(decodificado, 'html.parser')
